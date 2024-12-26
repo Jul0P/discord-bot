a13ba4f501d6da2ee9bb486a6c849a2d0b1fb493
Jul0P <jules.parents@gmail.com>
2024-12-26 23:15:01 +0100

    feat(commands): add clear command with channel and user options and update documentation for clear command

--- src/commands/documentation.js
+++ src/commands/documentation.js
@@ -4,53 +4,57 @@ export default {
 	data: new SlashCommandBuilder().setName("doc").setDescription("Documentation du bot"),
 
 	async execute(interaction) {
-		const embed = new EmbedBuilder().setTitle("Documentation du Bot").setDescription("Voici la liste des commandes disponibles et leurs descriptions").setColor("#0099ff").setFields(
-			{
-				name: "\u200B",
-				value: "**Rôles nécessaires : <@&1280508888206282812> & <@&1280508888206282810>**",
-				inline: false,
-			},
-			{
-				name: "/adddevoir `date` `groupe` `matiere` `description`",
-				value: "Ajouter un devoir",
-				inline: false,
-			},
-			{
-				name: "/deletedevoir `date` `groupe` `matiere`",
-				value: "Supprimer un devoir",
-				inline: false,
-			},
-			{
-				name: "/updatedevoir `date` `groupe` `matiere` `[description]` `[nouvelle_matiere]`",
-				value: "Mettre à jour un devoir",
-				inline: false,
-			},
-			{
-				name: "\u200B",
-				value: "**Rôle nécessaire : <@&1280508888206282812>**",
-				inline: false,
-			},
-			{
-				name: "/clear `nombre`",
-				value: "Supprimer des messages",
-				inline: false,
-			},
-			{
-				name: "\u200B",
-				value: "**Rôle nécessaire : @everyone**",
-				inline: false,
-			},
-			{
-				name: "/status",
-				value: "Envoie un message de status",
-				inline: false,
-			},
-			{
-				name: "/doc",
-				value: "Documentation du bot",
-				inline: false,
-			},
-		);
+		const embed = new EmbedBuilder()
+			.setTitle("Documentation du Bot")
+			.setDescription("Voici la liste des commandes disponibles et leurs descriptions")
+			.setColor("#0099ff")
+			.setFields(
+				{
+					name: "\u200B",
+					value: "**Rôles nécessaires : <@&1280508888206282812> & <@&1280508888206282810>**",
+					inline: false,
+				},
+				{
+					name: "/adddevoir `date` `groupe` `matiere` `description`",
+					value: "Ajouter un devoir",
+					inline: false,
+				},
+				{
+					name: "/deletedevoir `date` `groupe` `matiere`",
+					value: "Supprimer un devoir",
+					inline: false,
+				},
+				{
+					name: "/updatedevoir `date` `groupe` `matiere` `[description]` `[nouvelle_matiere]`",
+					value: "Mettre à jour un devoir",
+					inline: false,
+				},
+				{
+					name: "\u200B",
+					value: "**Rôle nécessaire : <@&1280508888206282812>**",
+					inline: false,
+				},
+				{
+					name: "/clear `nombre` `[salon]` `[utilisateur]`",
+					value: "Supprimer des messages",
+					inline: false,
+				},
+				{
+					name: "\u200B",
+					value: "**Rôle nécessaire : @everyone**",
+					inline: false,
+				},
+				{
+					name: "/status",
+					value: "Envoie un message de status",
+					inline: false,
+				},
+				{
+					name: "/doc",
+					value: "Documentation du bot",
+					inline: false,
+				},
+			);
 
 		await interaction.reply({ embeds: [embed] });
 	},
