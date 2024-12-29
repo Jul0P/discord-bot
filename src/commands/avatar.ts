35f933c9099cf25ea2e4f10583dde36a8bf0b6ba
Jul0P <jules.parents@gmail.com>
2024-12-29 17:51:10 +0100

    feat(commands): add avatar, status commands and update documentation for this commands

---
+++ src/commands/avatar.ts
@@ -0,0 +1,29 @@
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("avatar")
+		.setDescription("Récupère l'avatar d'un utilisateur.")
+		.addUserOption((option) =>
+			option
+				.setName("utilisateur")
+				.setDescription("L'utilisateur dont vous voulez récupérer l'avatar.")
+				.setRequired(true),
+		),
+
+	async execute(interaction: ChatInputCommandInteraction) {
+		const user = interaction.options.getUser("utilisateur");
+
+		if (!user) return;
+
+		const avatarURL = user.avatar
+			? user.displayAvatarURL({ extension: "gif", size: 4096 })
+			: user.displayAvatarURL({ extension: "png", size: 4096 });
+
+		const avatarEmbed = new EmbedBuilder()
+			.setDescription(`**Avatar de <@${user.id}>**`)
+			.setImage(avatarURL)
+			.setColor("#0099ff");
+		await interaction.reply({ embeds: [avatarEmbed] });
+	},
+};
